c922cfad7ccd96f1c1806d1ba3a9a593f256a663
Liviu Popa <liviucst@gmail.com>
2018-12-20 14:26:32 +0000

    Updated the project structure

---
+++ FractionLib/fractionlib.h
@@ -0,0 +1,13 @@
+#ifndef FRACTIONLIB_H
+#define FRACTIONLIB_H
+
+#include "fractionlib_global.h"
+
+class FRACTIONLIBSHARED_EXPORT FractionLib
+{
+
+public:
+    FractionLib();
+};
+
+#endif // FRACTIONLIB_H
